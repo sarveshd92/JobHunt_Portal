c8718934f8af61a5cfb581e16850f8b0e3aae0f8
sarvesh <dsarvesh92@gmail.com>
2025-06-09 22:24:13 +0530

    commit

---
+++ FrontEnd/dist/Contact.componenet.48eb3fb6.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire10c2;(0,e.register)("47KNK",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>i,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),i=()=>(0,a.jsx)("div",{children:(0,a.jsx)("h1",{className:" text-blue-400",children:"This is contact-us page"})})});
+//# sourceMappingURL=Contact.componenet.48eb3fb6.js.map
