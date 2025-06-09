a65adecb7497c102ac9b1e0ad285df515a0cfe62
sarvesh <dsarvesh92@gmail.com>
2025-06-09 23:27:13 +0530

    updated mongodb connections

--- BackEnd/Util/DB_Connect.util.js
+++ BackEnd/Util/DB_Connect.util.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const DB_Connect=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_DB_URL)
+        await mongoose.connect("mongodb+srv://dsarvesh92:5ZvloPsvXRPqTS03@sarvesh.j9g8q3d.mongodb.net")
         console.log("Database connected")
     } catch (error) {
         console.log("Database not connected ->",error.message)
